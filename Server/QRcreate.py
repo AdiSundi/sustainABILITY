cbb080edbcdfd56a941df6ace08968fed73eb469
Aditya Sundaresan <sundaresanaditya@gmail.com>
2021-02-28 10:22:45 +0800

    APP completed functionality

--- Server/QRcreate.py
+++ Server/QRcreate.py
@@ -1,13 +1,13 @@
 import qrcode
 
-def createQR(unique_id, sku_id, date_manufacture, plastic_type):
+def createQR(unique_id, sku_id_product_name, date_manufacture, plastic_type):
     qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4
     )
-    qr.add_data(str(unique_id) + " " + str(sku_id)+" "+str(date_manufacture)+" "+plastic_type)
+    qr.add_data(str(unique_id) + " " + str(sku_id_product_name)+" "+str(date_manufacture)+" "+plastic_type)
     qr.make(fit=True)
     img = qr.make_image()
     with open('prod_'+str(unique_id)+'.png', 'wb') as f:
